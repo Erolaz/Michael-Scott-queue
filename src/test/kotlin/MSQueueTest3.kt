9fe6e962eaaee7796f8fe68e3b245f59889600d1
Maksim Kudriashov <erolaz@outlook.com>
2024-12-05 05:57:50 +0300

    ModelChecking test

---
+++ src/test/kotlin/MSQueueTest3.kt
@@ -0,0 +1,30 @@
+package org.example
+
+import org.jetbrains.kotlinx.lincheck.annotations.*
+import org.jetbrains.kotlinx.lincheck.check
+import org.jetbrains.kotlinx.lincheck.strategy.stress.StressOptions
+import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingOptions
+import kotlin.test.Test
+import java.util.*
+
+class MSQueueTest3 {
+    private val q = MichaelScottQueue<Int>()
+
+    @Operation
+    fun enqueue() = q.enqueue(1)
+
+    @Operation
+    fun dequeue() = q.dequeue()
+
+    @Test
+    fun modelChecking() =
+        ModelCheckingOptions()
+            .check(this::class)
+
+    @Test
+    fun modelCheckingObstructionFreedom() =
+        ModelCheckingOptions()
+            .checkObstructionFreedom()
+            .check(this::class)
+
+}
